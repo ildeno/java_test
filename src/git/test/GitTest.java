570dad86a949f04367aa1bad79041ffd5b465418
denis <denis@localhost.localdomain>
2016-04-07 11:48:51 +0200

    GitTest.java file created and added first line of code

---
+++ src/git/test/GitTest.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package git.test;
+
+/**
+ *
+ * @author denis
+ */
+public class GitTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+    
+    
+    /**
+     * Just a printed message
+     * @param value 
+     */
+    private static void message(String value){
+        System.out.println(value);
+    }
+}
